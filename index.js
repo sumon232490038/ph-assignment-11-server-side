3ab449ceaaf5d088f88d83d7be5c9340031842cf
sumon232 <sumonmia4526@gmail.com>
2024-12-31 23:31:53 +0600

    Alhamdulillah receiving data from frontend by post method and add to the mongodb is done

--- index.js
+++ index.js
@@ -1,12 +1,43 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 const cors = require("cors");
-app.use(express());
+app.use(express.json());
 app.use(cors());
 
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xhl2h.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    const txDatabase = client.db("tutorXpressDB").collection("tutorials");
+
+    app.post("/addTutorials", async (req, res) => {
+      const tutorial = req?.body;
+      console.log(tutorial);
+      const result = await txDatabase.insertOne(tutorial);
+      res.send(result);
+    });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+  }
+}
+run().catch(console.dir);
+
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("TutorXpress........!");
 });
 
 app.listen(port, () => {
